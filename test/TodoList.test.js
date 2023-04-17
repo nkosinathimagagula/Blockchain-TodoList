3ce0931c5fff287e972f0df2e4e81ad58a48dbb0
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-04-17 08:52:43 +0200

    add test for listing the tasks correctly

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -16,4 +16,14 @@ contract('TodoList', (accounts) => {
         assert.notEqual(address, null)
         assert.notEqual(address, undefined)
     })
+
+    it('lists tasks', async () => {
+        const taskCount = await this.todoList.taskCount()
+        const task = await this.todoList.tasks(taskCount)
+
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.content, 'This todolist runs on the blockchain.')
+        assert.equal(task.completed, false)
+        assert.equal(taskCount.toNumber(), 1)
+    })
 })
